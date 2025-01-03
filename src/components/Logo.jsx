62fb7aba4ff92b16500286aa3e9a388eb7e32121
Dj-Artimus-X <Pratik.djArtimus@gmail.com>
2025-01-03 20:49:45 +0530

    responsive fixes and logo fixes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Logo = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlLogo = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY || window.scrollY > 20) {
+        console.log('scroll', window.scrollY)
+        // if scroll down hide the logo
+        setIsVisible(false);
+      } else {
+        // if scroll up show the logo
+        setIsVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlLogo);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlLogo);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-        <div className="fixed h-44 z-50 hidden">
-          <div className=" relative h-full border">
-            <div className="h-28 w-full border"></div>
-            <div className=" z-50">
-                  <h1 className="sticky top-0 left-[12vw] text-[3vw] w-fit font-extrabold text-primary font-dancingScript -rotate-12 bo rder backdrop-blur-sm bg-black bg-opacity-50 rounded-full leading-none">
-                    DjArtimus
-                  </h1>
-            </div>
-          </div>
-        </div>
+    <div
+      className={`fixed h-fit w-fit z-50 right-[8vw] top-[9vw] lg:top-[10vh] sm:right-auto sm:left-[12vw] transition-all duration-1000 ${
+        isVisible ? "translate-y-0 text-[10vw]" : " text-[7vw] -translate-y-[4vh] sm:-translate-y-[8vh]"
+      }`}
+    >
+      <h1 className="lg:text-[3vw] w-fit font-extrabold text-blue-500 font-dancingScript rotate-6 sm:-rotate-12 bor der backdrop-blur-xl p-2 rounded-full leading-none">
+        DjArtimus
+      </h1>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,58 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
-import ThemeToggle from "./ThemeToggle";
 import "../app/globals.css";
 
-const Navbar = ({ onThemeToggle, isDarkMode }) => (
-  <nav className="hidden fixed right-[12vw] lg:top-[0.3vh] 2xl:top-[1vh] z-50 bor der h-[4vh] lg:flex flex-col items-center bg-black bg-opacity-5 backdrop-blur-xl rounded-full">
-    <div className="flex gap-[2vh] items-center px-[1vw]">
-      {["home", "about", "skills", "projects", "contact"].map((section) => (
-        <Link
-          key={section}
-          activeClass="text-primary"
-          to={section}
-          spy={true}
-          smooth={true}
-          duration={500}
-          className="cursor-pointer hover:text-sky-200 hover:font-bold text-[1.2vw] hover:text-[1.3vw] underline-offset-4  hover:un derline transition-all duration-300"
-        >
-          {section.charAt(0).toUpperCase() + section.slice(1)}
-        </Link>
-      ))}
-    </div>
-  </nav>
-);
+const Navbar = ({ onThemeToggle, isDarkMode }) => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setIsVisible(false);
+      } else {
+        // if scroll up show the navbar
+        setIsVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
+  return (
+    <nav
+      className={`fixed hidden right-[12vw] lg:top-[0.3vh] 2xl:top-[1vh] z-50 border h-[4vh] lg:flex flex-col items-center bg-black bg-opacity-5 backdrop-blur-xl rounded-full transition-transform duration-300 ${
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <div className="flex gap-[2vh] items-center px-[1vw]">
+        {["home", "about", "skills", "projects", "contact"].map((section) => (
+          <Link
+            key={section}
+            activeClass="text-primary"
+            to={section}
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="cursor-pointer hover:text-sky-200 hover:font-bold text-[1.2vw] hover:text-[1.3vw] underline-offset-4 hover:underline transition-all duration-300"
+          >
+            {section.charAt(0).toUpperCase() + section.slice(1)}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
