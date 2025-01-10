d20a6e7d86c1addffae5072b94f6c8b10ebfd179
Dj-Artimus-X <Pratik.djArtimus@gmail.com>
2025-01-11 03:37:18 +0530

    Final Deployment

--- src/utils/contactMessageSender.js
+++ src/utils/contactMessageSender.js
@@ -7,15 +7,15 @@ export const sendContactMessage = async (email, message) => {
     const transporter = nodemailer.createTransport({
       service: "gmail", // You can also use other SMTP services
       auth: {
-        user: process.env.NEXT_PUBLIC_EMAIL_USER, // Your email address
-        pass: process.env.NEXT_PUBLIC_EMAIL_PASS, // Your email's app password
+        user: process.env.EMAIL_USER, // Your email address
+        pass: process.env.EMAIL_PASS, // Your email's app password
       },
     });
 
     // Email options
     const mailOptions = {
-      from: process.env.NEXT_PUBLIC_EMAIL_USER,
-      to: process.env.NEXT_PUBLIC_EMAIL_TO, // Receiver's email
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_TO, // Receiver's email
       subject: `Contact Msg from ${message.split(',')[0]}`,
       text: `From: ${email}\n\nMessage:\n\n${message}`,
     };
